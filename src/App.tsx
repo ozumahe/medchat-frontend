4f6894f535acffe92e16fafe4a075f15f1572aea
ozumahe <ozumahe@gmail.com>
2022-08-28 22:47:30 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from "react";
+import "./App.css";
 
-function App() {
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelContainer, ChannelListContainer } from "./components";
+
+interface Props {}
+
+const App: FC<Props> = ({}: Props) => {
+  const apiKey = "w5ubfwttser2s";
+  const client = StreamChat.getInstance(apiKey);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelContainer />
+        <ChannelListContainer />
+      </Chat>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ChannelContainer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ChannelContainer = (props: Props) => {
+  return <div>ChannelContainer</div>;
+};
+
+export default ChannelContainer;
