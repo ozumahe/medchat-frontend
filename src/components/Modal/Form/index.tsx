d34948caa017ff8f5a20be9bf691f2992e9e5bbe
ozumahe <ozumahe@gmail.com>
2022-09-04 21:00:34 +0100

    update

--- src/components/Modal/Form/index.tsx
+++ src/components/Modal/Form/index.tsx
@@ -11,6 +11,7 @@ import {
   MenuList,
   MenuItem,
   Flex,
+  useToast,
 } from "@chakra-ui/react";
 import { location, equipments, equipmentType } from "data";
 import { DropDownIcon } from "svgIcons";
@@ -33,9 +34,26 @@ const statusData: StatusData[] = [
 
 const Form = ({ isOpen, onClose }: Props) => {
   const [isRequest, setIsRequest] = useState(false);
-  const [systemTypeId, setSystemTypeId] = useState(null);
-  const [equipmentTypeId, setEquipmentTypeId] = useState(null);
-  const [sbuId, setSbuId] = useState(null);
+  const toast = useToast();
+
+  const [systemTypeId, setSystemTypeId] = useState({
+    id: null,
+    systemType: "",
+  });
+
+  const [equipmentTypeId, setEquipmentTypeId] = useState<{
+    id: any;
+    equipmentType: "";
+  }>({
+    id: null,
+    equipmentType: "",
+  });
+
+  const [sbuId, setSbuId] = useState({
+    id: null,
+    sub: "",
+  });
+
   const [equipmentId, setEquipmentId] = useState<{
     id: any;
     equipment: string;
@@ -43,14 +61,29 @@ const Form = ({ isOpen, onClose }: Props) => {
     id: null,
     equipment: "",
   });
+
   const [status, setStatus] = useState(null);
-  const [locationId, setLocationId] = useState(null);
+
+  const [locationId, setLocationId] = useState<{ id: any; location: string }>({
+    id: null,
+    location: "",
+  });
 
   const handleSubmit = () => {
+    if (!locationId.id || !equipmentId.id || !equipmentTypeId.id) {
+      return toast({
+        status: "error",
+        description: "provide reqiued Inputs",
+        duration: 2000,
+        position: "top",
+      });
+    }
+
     setIsRequest(true);
 
     setTimeout(() => {
       setIsRequest(false);
+      onClose();
     }, 2000);
   };
 
@@ -61,10 +94,46 @@ const Form = ({ isOpen, onClose }: Props) => {
       heading="Maintenance Input"
       headingColor="#000"
     >
-      <SelectUI label="Location" options={location} />
+      {/* <SelectUI label="Location" options={location} /> */}
       {/* <SelectUI label="Equipment" /> */}
       {/* <SelectUI label="Equipment Type" /> */}
       {/* <SelectUI label="Status" /> */}
+      <Box mt="15px">
+        <Text textTransform="capitalize">Location</Text>
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton
+                mt="5px"
+                h="70px"
+                w="100%"
+                border="1px solid gray"
+                borderRadius="8px"
+                px="20px"
+              >
+                <Flex justifyContent="space-between" alignItems="center">
+                  <Text>
+                    {locationId.location ? locationId.location : "Location"}
+                  </Text>
+                  <Box transform={isOpen ? "rotate(180deg)" : ""}>
+                    <DropDownIcon />
+                  </Box>
+                </Flex>
+              </MenuButton>
+              <MenuList w="300px">
+                {location.map(({ id, name }: { id: string; name: string }) => (
+                  <MenuItem
+                    key={id}
+                    onClick={() => setLocationId({ id: id, location: name })}
+                  >
+                    {name}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </>
+          )}
+        </Menu>
+      </Box>
       <Box mt="15px">
         <Text textTransform="capitalize">Equipment</Text>
         <Menu>
@@ -148,7 +217,11 @@ const Form = ({ isOpen, onClose }: Props) => {
                 px="20px"
               >
                 <Flex justifyContent="space-between" alignItems="center">
-                  <Text>Equipment Type</Text>
+                  <Text>
+                    {equipmentTypeId.equipmentType
+                      ? equipmentTypeId.equipmentType
+                      : "Equipment Type"}
+                  </Text>
                   <Box transform={isOpen ? "rotate(180deg)" : ""}>
                     <DropDownIcon />
                   </Box>
@@ -156,7 +229,14 @@ const Form = ({ isOpen, onClose }: Props) => {
               </MenuButton>
               <MenuList w="300px">
                 {equipmentType.map(({ id, systemType }: any) => (
-                  <MenuItem key={id}>{systemType.name}</MenuItem>
+                  <MenuItem
+                    key={id}
+                    onClick={() =>
+                      setEquipmentTypeId({ id, equipmentType: systemType.name })
+                    }
+                  >
+                    {systemType.name}
+                  </MenuItem>
                 ))}
               </MenuList>
             </>
